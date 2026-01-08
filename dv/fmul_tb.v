cd25ea0181c7e477779e41d74246f08218e3d25e
Mia Solak <miasolak@gmail.com>
2026-01-08 21:32:57 +0100

    Fixed when number is zero

---
+++ dv/fmul_tb.v
@@ -0,0 +1,42 @@
+module fmul_tb;
+
+    reg [31:0] a_i;
+    reg [31:0] b_i;
+    wire [31:0] c_o;
+    
+    //Instanciranje DUT-a
+    fmul dut (
+    .a_i (a_i),
+    .b_i (b_i),
+    .c_o (c_o)
+    );
+    
+    initial begin
+        a_i = 32'h3F800000;   // 1.0
+        b_i = 32'h3F800000;   // 1.0
+        #10;
+        //expected = 32'h3F800000
+
+        a_i = 32'h3F800000;   // 1.0
+        b_i = 32'h40000000;   // 2.0
+        #10;
+        //expected = 32'h40000000
+        
+        a_i = 32'h3FC00000;   // 1.5
+        b_i = 32'h40000000;   // 2.0
+        #10;
+        //expected = 32'h40400000
+        
+        a_i = 32'hBF800000;   // -1.0
+        b_i = 32'h40000000;   // 2.0
+        #10;
+        //expected = 32'hC0000000
+        
+        a_i = 32'h00000000;   // 0
+        b_i = 32'h40000000;   // 2.0
+        #10;
+        //expected = 32'h00000000
+    
+    end     
+    
+endmodule
--- rtl/fmul.v
+++ rtl/fmul.v
@@ -17,43 +17,44 @@ module fmul(
 //      sign_c = a_i[31] ^ b_i[31];
 //    end
     assign bias = 127;
+    assign c_o = (a_i == 0 || b_i == 0) ? 'h0 : {sign_c, exp_c, mant_c};
+    
     always @(*)begin
         exp_c = a_i[30:23] + b_i[30:23] - bias;
         pom_mant = {1'b1, a_i[22:0]} * {1'b1, b_i[22:0]};
-        if (pom_mant[47] == 1'b1) begin //razmisli za uslov
+        if (pom_mant[47] == 1'b1) begin //10 xx normalizacija
             pom_mant = pom_mant >> 1;     //sad je pom sigurno oblika: 01 46bita
             exp_c = exp_c + 1;
         end
-        if (pom_mant[22] == 1'b0)begin
+        if (pom_mant[22] == 1'b0)begin  //01 xx
             mant_c = pom_mant[45:23];
-        end else if (pom_mant[22] == 1'b1)begin
+        end else if (pom_mant[22] == 1'b1)begin //odbaceni bit 1?
             if (|pom_mant[21:0] == 1'b1) begin  //bar jedna jedinica
-                mant_c = {pom_mant[45:24], 1'b1};
+                mant_c = {pom_mant[45:24], 1'b1}; //na vise
                 
-                if (mant_c[22] == 1'b1) begin
+                if (mant_c[22] == 1'b1) begin   // normalizacija
                     mant_c = mant_c >> 1;
                     exp_c = exp_c + 1;
                 end
                 
-            end else if (|pom_mant[21:0] != 1'b1) begin
-                if (pom_mant[23] == 1'b1) begin
-                    mant_c = {pom_mant[45:24], 1'b1};
+            end else if (|pom_mant[21:0] != 1'b1) begin //nema nijedne jedinice
+                if (pom_mant[23] == 1'b1) begin //poslednji bit 1?
+                    mant_c = {pom_mant[45:24], 1'b1}; //na vise
                     
-                    if (mant_c[22] == 1'b1) begin
+                    if (mant_c[22] == 1'b1) begin   //normalizacija
                         mant_c = mant_c >> 1;
                         exp_c = exp_c + 1;
                     end
                     
-                end else if (pom_mant[23] == 1'b0) begin
+                end else if (pom_mant[23] == 1'b0) begin //poslednji bit 0?
                     mant_c = pom_mant[45:23];
                 end
            end
-        end
-        
-    end
+         end
+     end
 
     
-    assign c_o = {sign_c, exp_c, mant_c};
+
     
     
 endmodule
